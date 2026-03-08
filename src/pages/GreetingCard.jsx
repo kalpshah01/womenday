673d886235a169e72c3f7bf6a4bf92b1d9abc850
kalpshah01 <kalpshah962005@gmail.com>
2026-03-08 17:05:16 +0530

    adding name

--- src/pages/GreetingCard.jsx
+++ src/pages/GreetingCard.jsx
@@ -18,7 +18,8 @@ const NAME_MESSAGES = {
     hetvi: 'Hi Hetvi 💕 You light up lives just by being you. always helpful to me ',
     mummy: 'Hi Mummy 🌹 You are the heart and soul of our family, always loving and caring.',
     aarti: 'Hi Aarti 🌼 Your kindness and warmth make you truly special. You are a wonderful person. always understand and our friendship is unexpected like god send people speically for us like that',
-    
+    preksha: 'Hi Preksha 🌼 you are my sister i am always there for you sister remeber this just live life like no one expected ',
+
 }
 
 const DEFAULT_MSG = 'Happy Women\'s Day 🌸 You are amazing and inspiring.'
